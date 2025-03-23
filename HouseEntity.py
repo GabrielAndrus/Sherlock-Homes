7a00d4070257cc6b1bd04932c56761cc00d0949f
zahrabuch <zahra.buchanan@mail.utoronto.ca>
2025-03-23 18:14:14 -0500

    Create HouseEntity.py
    House Class

---
+++ HouseEntity.py
@@ -0,0 +1,32 @@
+"""Defining House"""
+from dataclasses import dataclass
+
+
+@dataclass
+class House:
+    """House data class"""
+    ward: str
+    beds: int
+    baths: int
+    DEN: bool
+    size: tuple[int, int]
+    parking: bool
+    building_age: int
+    maint: int
+    price: int
+    location: tuple[int, int]
+
+    def __init__(self, ward: str, beds: int, baths: int, size: tuple[int, int], building_age: int,
+                 maint: int, price: int, location: tuple[int, int], den=False, parking=False,) -> None:
+        """Initialize a new house.
+        """
+        self.ward = ward
+        self.beds = beds
+        self.baths = baths
+        self.DEN = den
+        self.size = size
+        self.parking = parking
+        self.maint = maint
+        self.building_age = building_age
+        self.price = price
+        self.location = location
