61632a80714feea346dbb9b16d06c2863976008a
zahrabuch <zahra.buchanan@mail.utoronto.ca>
2025-03-23 19:42:57 -0500

    Update HouseEntity.py

--- HouseEntity.py
+++ HouseEntity.py
@@ -20,7 +20,7 @@ class House:
                  maint: int, price: int, location: tuple[int, int], den=False, parking=False,) -> None:
         """Initialize a new house.
         """
-        self.int = id
+        self.id = id
         self.beds = beds
         self.baths = baths
         self.DEN = den
