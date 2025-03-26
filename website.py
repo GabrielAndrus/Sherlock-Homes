4b647d7cc69d8d6749efb4f93032607c9fa10440
deepankargarg <gargdeepankar0202@gmail.com>
2025-03-26 15:21:55 -0400

    basic website

--- website.py
+++ website.py
@@ -1,1 +1,12 @@
-pip install
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "This is the home page"
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
