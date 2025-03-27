5f2be2fca94787f824a5a8266392ef1369d9eef5
deepankargarg <gargdeepankar0202@gmail.com>
2025-03-27 15:20:24 -0400

    m

--- website.py
+++ website.py
@@ -1,12 +1,9 @@
 from flask import Flask
-
+from views import chatbot, home
 app = Flask(__name__)
 
-
-@app.route("/")
-def home():
-    return "This is the home page"
-
+app.register_blueprint(home, url_prefix="/home")
+app.register_blueprint(chatbot, url_prefix="/chatbot")
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    app.run(debug=True, port=9000)
